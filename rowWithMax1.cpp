cf53ad25897676d483a5178f205254b556e0d597
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2021-09-10 22:10:05 +0530

    Row with max 1s

---
+++ rowWithMax1.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	   // int maxSum = 0, pos=-1;
+	   // for(int i = 0; i < n; i++){
+	   //     int sum = 0;
+	   //     for(int j = 0; j < m; j++){
+	   //         sum += arr[i][j];
+	   //     }
+	   //     if(sum > maxSum){
+	   //         pos = i;
+	   //         maxSum = sum;
+	   //     }
+	   // }
+	   // return pos;
+	   int i = 0;
+	   int ans = -1;
+	   int j = m-1;
+	   while(i < n && j > -1){
+	       if(arr[i][j] == 1){
+	            ans = i;
+	            j--;
+	       }
+	       if(arr[i][j] == 0)
+	        i++;
+	   }
+	   return ans;
+	}
+
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
